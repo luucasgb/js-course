0074205c41f308601f5e689fbf9ae4c762a3b2dc
luucasgb <luucasguimaraesb@gmail.com>
2022-07-03 22:46:16 -0300

    LECTURE: Data Types

--- 01-Fundamentals-Part-1/mine/assignments.js
+++ 01-Fundamentals-Part-1/mine/assignments.js
@@ -1,3 +1,6 @@
+//JavaScript Fundamentals - Part 1
+
+// Values and Variables
 let country = "Brazil";
 let continent = "South America";
 let population = "212.6 million";
@@ -5,3 +8,29 @@ let population = "212.6 million";
 console.log(`Country: ${country}`);
 console.log(`Continent: ${continent}`);
 console.log(`Population: ${population}"`);
+
+// Data Types
+let isIsland = false;
+let language;
+console.log(`${typeof isIsland}`);
+console.log(`${typeof population}`);
+console.log(`${typeof country}`);
+console.log(`${typeof language}`);
+
+// let, const and var
+
+// Basic operators
+
+// Strings and template literals
+
+// Taking decisions: if/else statements
+
+// Type conversion and coercion
+
+// Equality operators: == vs ===
+
+// Logical operators
+
+// Switch statement
+
+// Ternary operator
