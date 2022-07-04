7fb580d96dbd2a5507479f49e5d7e5fbf5822c55
luucasgb <luucasguimaraesb@gmail.com>
2022-07-03 22:36:03 -0300

    LECTURE: Values and Variables

---
+++ 01-Fundamentals-Part-1/mine/assignments.js
@@ -0,0 +1,7 @@
+let country = "Brazil";
+let continent = "South America";
+let population = "212.6 million";
+
+console.log(`Country: ${country}`);
+console.log(`Continent: ${continent}`);
+console.log(`Population: ${population}"`);
---
+++ 01-Fundamentals-Part-1/mine/script.js
@@ -0,0 +1,9 @@
+alert("hello world.");
+
+let js = "amazing";
+if (js === "amazing") alert("JavaScript is FUN!");
+console.log(js);
+
+js = "boring";
+if (js === "amazing") alert("JavaScript is FUN!");
+console.log(js);
