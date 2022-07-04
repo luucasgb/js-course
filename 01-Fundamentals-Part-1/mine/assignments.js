18ade6ea1b14d6fecc0f8698dd5b676433bbf511
luucasgb <luucasguimaraesb@gmail.com>
2022-07-04 00:25:01 -0300

    LECTURE: The switch Statement

--- 01-Fundamentals-Part-1/mine/assignments.js
+++ 01-Fundamentals-Part-1/mine/assignments.js
@@ -85,5 +85,26 @@ if (language === "English" && population < 50000000 && !isIsland) {
 }
 
 // Switch statement
+switch (language) {
+  case "Chinese":
+  case "Mandarin":
+    console.log("MOST number of native speakers!");
+    break;
+  case "Spanish":
+    console.log("2nd place in number of native speakers.");
+    break;
+  case "English":
+    console.log("3rd place.");
+    break;
+  case "Hindi":
+    console.log("Number 4");
+    break;
+  case "Arabic":
+    console.log("5th most spoken language");
+    break;
+  default:
+    console.log("Great language too.");
+    break;
+}
 
 // Ternary operator
