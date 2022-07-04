000fc1a71e9293d58be8dd4346c11db5fd072ded
luucasgb <luucasguimaraesb@gmail.com>
2022-07-03 23:06:37 -0300

    LECTURE: Basic Operators and Operator Precedence

--- 01-Fundamentals-Part-1/mine/assignments.js
+++ 01-Fundamentals-Part-1/mine/assignments.js
@@ -1,16 +1,16 @@
 //JavaScript Fundamentals - Part 1
 
 // Values and Variables
-let country = "Brazil";
-let continent = "South America";
-let population = "212.6 million";
+const country = "Brazil";
+const continent = "South America";
+let population = 212600000;
 
 console.log(`Country: ${country}`);
 console.log(`Continent: ${continent}`);
-console.log(`Population: ${population}"`);
+console.log(`Population: ${population}`);
 
 // Data Types
-let isIsland = false;
+const isIsland = false;
 let language;
 console.log(`${typeof isIsland}`);
 console.log(`${typeof population}`);
@@ -18,8 +18,26 @@ console.log(`${typeof country}`);
 console.log(`${typeof language}`);
 
 // let, const and var
+// let -> mutable
+language = "Portuguese";
+// const -> immutable
+// continent = "Europe";
 
 // Basic operators
+let halfPopulation = population / 2;
+console.log(`Half of Brazil's population is ${halfPopulation}`);
+console.log(population++);
+const largerThanFinland = population > 6000000;
+console.log(
+  `Is Brazil's population larger than Finland's? ${largerThanFinland}`
+);
+const smallerThanAverage = population < 33000000;
+console.log(
+  `Is Brazil's population smaller than the average country population? ${smallerThanAverage}`
+);
+console.log(
+  `${country} is in ${continent}, and its ${population} people speak ${language}`
+);
 
 // Strings and template literals
 
